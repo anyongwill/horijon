d90ac2bbb9084ea7d6818374a9cda11c6bedc4c1
anyongwill <anyongwill@gmail.com>
2024-11-29 23:24:43 -0500

    Updated grid layout and package.json for Netlify

--- src/components/sections/Team/Team.jsx
+++ src/components/sections/Team/Team.jsx
@@ -27,13 +27,13 @@ const Team = () => {
       image: "./Images/Team Headshot/Gabby.jpg",
       title: "Gabby Vukomanovic",
       subtitle: "Engineer",
-      description: "Front-end application developer. Computer Science major at Brandies University (Dean's list, multiple awards). Led development of multiple 3D/Gaming projects"
+      description: "Front-end application developer. Computer Science major at Brandeis University (Dean's list, multiple awards). Led development of multiple 3D/Gaming projects"
     },
     {
       image: "./Images/Team Headshot/Delanie.png",
       title: "Delanie Pon",
       subtitle: "Designer",
-      description: "Illustrator and Graphic Designer. Animation major at Massachussets College of Art and Design (Honor student, multiple awards). Leader of MassArt Game Design Club."
+      description: "Illustrator and Graphic Designer. Animation major at Massachusetts College of Art and Design (Honor student, multiple awards). Leader of MassArt Game Design Club."
     },
     {
       image: "./Images/Team Headshot/Masaki.jpg", 
